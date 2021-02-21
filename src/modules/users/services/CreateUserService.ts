d10203825e65930c7f4b1d11e2447d74b68ae309
Gabriel Paolini <paolini396@gmail.com>
2021-02-20 22:01:52 -0300

    application DDD

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,40 @@
+import { hash } from 'bcryptjs'
+import { injectable, inject } from 'tsyringe';
+import AppError from '@shared/errors/AppError';
+
+import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUserRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+@injectable()
+class CreateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({name, email, password}: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
+
+    if (checkUserExists) {
+      throw new AppError('Este e-mail já está sendo utilizado.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = await this.usersRepository.create({
+      email,
+      name,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
+
+export default CreateUserService;
