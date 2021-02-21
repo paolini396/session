863bbfc41416675d39e8c561eeccc19e90610fb8
Gabriel Paolini <paolini396@gmail.com>
2021-02-21 16:33:31 -0300

    feat: session User.

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '52c69e3a57331081823331c4e69d3f2e',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import AutenticateUserService from '@modules/users/services/AutenticateUserService';
+
+class SessionsController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+
+    const authenticateUser = container.resolve(AutenticateUserService);
+
+    const { user, token } = await authenticateUser.execute({ email, password })
+
+    delete user.password;
+
+    return response.json({ user, token })
+  }
+}
+
+export default SessionsController;
