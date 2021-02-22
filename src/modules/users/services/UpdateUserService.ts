3e1e5d5f08d410159a97a35ed678bcaee8e90759
Gabriel Paolini <paolini396@gmail.com>
2021-02-21 22:31:58 -0300

    feat: users CRUD

--- src/modules/users/services/UpdateUserService.ts
+++ src/modules/users/services/UpdateUserService.ts
@@ -11,7 +11,7 @@ interface IRequest {
 }
 
 @injectable()
-class CreateUserService {
+class UpdateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
@@ -32,4 +32,4 @@ class CreateUserService {
   }
 }
 
-export default CreateUserService;
+export default UpdateUserService;
